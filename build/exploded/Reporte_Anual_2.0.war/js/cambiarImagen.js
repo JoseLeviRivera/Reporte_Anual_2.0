159f0fab9617473ace04ba1b0d5bb878c08c6a18
JoseLeviRivera <joselevirivera@gmail.com>
2022-06-26 16:10:21 -0500

    Agrego el diseño del login

---
+++ build/exploded/Reporte_Anual_2.0.war/js/cambiarImagen.js
@@ -0,0 +1,14 @@
+const interval = setInterval(() => {
+	let position = random(1,3);
+	switch(position){
+		case 1: document.getElementById('imagen-ventana').src="img/1.jpg"; break;
+		case 2: document.getElementById('imagen-ventana').src="img/2.jpg"; break;
+		case 3: document.getElementById('imagen-ventana').src="img/3.jpg"; break;
+		default: document.getElementById('imagen-ventana').src="img/1.jpg"; break;	
+	}
+},3000);
+
+let random = (min, max) => {
+    return Math.floor((Math.random() * (max - min + 1)) + min); 
+}
+
